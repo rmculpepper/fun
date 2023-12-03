a2857de0c330d77b8bbc6d6ec499640dc33df0a8
Ryan Culpepper <ryanc@racket-lang.org>
2023-12-03 00:59:56 -0500

    day 3

--- aoc2023/day03.rkt
+++ aoc2023/day03.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 ;; symbol-locs : Hash[Nat => Hash[Nat => (U #t (Box (Listof Nat)))]]
+;; Map line index to hash that maps char index to box (for gear) or #t (else).
 (define symbol-locs (make-hash))
 
 ;; set-symbol! : Nat Nat Boolean -> Void
@@ -15,6 +16,7 @@
   (define lih (hash-ref symbol-locs li #f))
   (and lih (hash-ref lih ci #f)))
 
+;; Pass1: Note all symbol locations, create boxes for each gear.
 
 ;; pass1 : (Listof String) -> Void
 (define (pass1 lines)
@@ -37,6 +39,9 @@
   (for/or ([sc (in-string "~!@#$%^&*_+-=/")]) ;; ???
     (eqv? c sc)))
 
+;; Pass2: For each number, check for adjacent symbol.
+;; If adjacent to gear, add to gearbox.  Note: avoid short-circuiting `or`
+;; in adjacency check, else might miss adding number to gearbox.
 
 ;; pass2 : (Listof String) -> Nat
 (define (pass2 lines)
@@ -48,22 +53,34 @@
   (let loop ([start 0])
     (match (regexp-match-positions #rx"[0-9]+" line start)
       [(list (cons nstart nend))
-       (+ (if (adjacent-to-symbol? li nstart nend)
-              (string->number (substring line nstart nend))
-              0)
+       (define n (string->number (substring line nstart nend)))
+       (+ (if (adjacent-to-symbol li nstart nend n) n 0)
           (loop nend))]
       [#f 0])))
 
-;; adjacent-to-symbol? : Nat Nat Nat -> Boolean
-(define (adjacent-to-symbol? li cstart cend)
-  (or
-   ;; check same line
-   (or (check-symbol li (sub1 cstart))
-       (check-symbol li cend))
-   ;; check lines above and below
-   (for/or ([check-li (in-list (list (sub1 li) (add1 li)))])
-     (for/or ([check-ci (in-range (sub1 cstart) (add1 cend))])
-       (check-symbol check-li check-ci)))))
+;; adjacent-to-symbol : Nat Nat Nat Nat -> Boolean
+;; Returns #t if adjacent to symbol. Adds n to gearbox of adjacent gear.
+(define (adjacent-to-symbol li cstart cend n)
+  ;; reduce : (Listof (U Boolean (Box (Listof Nat)))) -> Boolean
+  (define (reduce v)
+    (define vs (flatten v))
+    (for ([b (in-list vs)] #:when (box? b))
+      (set-box! b (cons n (unbox b))))
+    (and (ormap values vs) #t))
+  (reduce (list
+           ;; check same line
+           (list (check-symbol li (sub1 cstart))
+                 (check-symbol li cend))
+           ;; check lines above and below
+           (for/list ([check-li (in-list (list (sub1 li) (add1 li)))])
+             (for/list ([check-ci (in-range (sub1 cstart) (add1 cend))])
+               (check-symbol check-li check-ci))))))
+
+;; pass3 : -> Nat
+(define (pass3)
+  (for/sum ([(li lih) (in-hash symbol-locs)])
+    (for/sum ([(ci b) (in-hash lih)] #:when (box? b))
+      (if (= 2 (length (unbox b))) (apply * (unbox b)) 0))))
 
 (module+ test
   (require rackunit)
@@ -81,9 +98,11 @@
       ".664.598.."))
 
   (pass1 example)
-  (check-equal? (pass2 example) 4361))
+  (check-equal? (pass2 example) 4361)
+  (check-equal? (pass3) 467835))
 
 (module+ main
   (define lines (port->lines))
   (pass1 lines)
-  (pass2 lines))
+  (pass2 lines)
+  (pass3))
